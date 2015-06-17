98422665783697c0bf155bc28050b4fcfc466afd
HTDS <htekgulds@gmail.com>
2015-06-18 00:23:06 +0300

    initial commit

---
+++ client/home.js
@@ -0,0 +1,31 @@
+function otherId(game) {
+    return game.currentTurn[ game.currentTurn[0] === Meteor.userId() ? 1 : 0 ];
+}
+
+Template.gameList.helpers({
+    games: function () {
+        return Games.find({ inProgress: true}).map(function (game) {
+            game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+            game.started = moment(game.started).fromNow();
+            return game;
+        });
+    }
+});
+
+Template.userList.helpers({
+    users: function () {
+        var myId = Meteor.userId();
+        var cantPlayAgainst = [myId];
+
+        Games.find({inProgress: true}).forEach(function (game) {
+            cantPlayAgainst.push(otherId(game));
+        });
+        return Meteor.users.find({ _id: { $not: { $in: cantPlayAgainst }}});
+    }
+});
+
+Template.userItem.events({
+    'click button': function (evt, template) {
+        Meteor.call('createGame', template.data._id);
+    }
+});
